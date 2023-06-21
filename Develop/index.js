f7ca54cdc39ece1837d87b5b94a61aaba207cbc0
jrdnwlkr <walker.jordan.m@gmail.com>
2023-06-21 16:13:39 -0500

    initial commit

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,67 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util')
+const mdu = require('markdown-utils');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [   
+    {
+       type: 'input',
+       message: 'What is the title of your project?',
+       name: 'title', 
+    },
+    {
+       type: 'input',
+       message: 'Describe your project.',
+       name: 'description', 
+    },
+    // {
+    //     type: 'input',
+    //     message: 'What are the steps required to install your project?',
+    //     name: 'installation',
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'How should your project be used?',
+    //     name: 'usage',
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'Describe an example of how your project can or should be used.',
+    //     name: 'example'
+    // },
+    // {
+    //     type: 'input',
+    //     message: 'Who should be credited for this project?',
+    //     name: 'credits',
+    // }
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// .then(response => {
+//         console.log(response);
+//         fs.writeFileSync("README.md", generateMarkdown(response));
+//         const { title, description, installation, usage, example, credits } = response;
+//         console.log(title, description, installation, usage, example, credits);
+//     });
+const answers = "";
+// // TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile("README.md", answers, (err) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(answers)
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(answers => {
+        console.log(answers);
+        writeToFile(answers);
+    })
+};
 
 // Function call to initialize app
 init();
